655dd203b1decb46d0d0cb9775e5fefc223aed77
Thiago <thiagocclima@gmail.com>
2025-05-13 21:56:53 +0100

    easytask project

---
+++ easytask/src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  selectedUser = DUMMY_USERS[0];
+
+}
