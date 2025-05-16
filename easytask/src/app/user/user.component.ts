a1576c455bc0775faa7d80e33de788b7f0c21ee8
Thiago <thiagocclima@gmail.com>
2025-05-16 16:19:02 +0100

    easytask: flexible user component

--- easytask/src/app/user/user.component.ts
+++ easytask/src/app/user/user.component.ts
@@ -1,9 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
-// To use Signals, import signal from angular/core
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,21 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) avatar !: string;
+  @Input({required: true}) name !: string;
 
-  // initialize a signal by passing an object
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-
-  // computed values with signals
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  // get imagePath() {
-  //   // the properties of the signal object must be called as a function
-  //   return 'assets/users/' + this.selectedUser().avatar
-  // }
-
-  onSelectUser() {
-    // to update the signal object, call the set(obj) method
-    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+  get imagePath() {
+    return 'assets/users/' + this.avatar
   }
+
+  onSelectUser() {}
 
 }
