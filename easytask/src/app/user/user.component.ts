0637310e73eb9ddb3962526b493f9c4857a2ac8a
Thiago <thiagocclima@gmail.com>
2025-05-16 14:54:15 +0100

    add signals to user component

--- easytask/src/app/user/user.component.ts
+++ easytask/src/app/user/user.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+// To use Signals, import signal from angular/core
 
 import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,6 +14,20 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
 
-  selectedUser = DUMMY_USERS[0];
+  // initialize a signal by passing an object
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // computed values with signals
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // get imagePath() {
+  //   // the properties of the signal object must be called as a function
+  //   return 'assets/users/' + this.selectedUser().avatar
+  // }
+
+  onSelectUser() {
+    // to update the signal object, call the set(obj) method
+    this.selectedUser.set(DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]);
+  }
 
 }
